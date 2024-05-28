e5b927ea9bfdba2fa0683746308b2264e770ff76
GABRIEL <gabriel.ruiz@jprconsulting.com.mx>
2024-05-28 16:49:11 -0600

    inmuebles imagen

--- src/app/pages/panel/inmuebles/inmuebles.component.ts
+++ src/app/pages/panel/inmuebles/inmuebles.component.ts
@@ -8,12 +8,12 @@ import { LoadingStates } from 'src/app/global/global';
 @Component({
   selector: 'app-inmuebles',
   templateUrl: './inmuebles.component.html',
-  styleUrls: ['./inmuebles.component.css']
+  styleUrls: ['./inmuebles.component.css'],
 })
 export class InmueblesComponent {
   @ViewChild('searchItem') searchItem!: ElementRef;
   @ViewChild('closebutton') closebutton!: ElementRef;
-
+  public isUpdatingfoto: boolean = false;
   inmueblesForm!: FormGroup;
   isLoading = LoadingStates.neutro;
   idUpdate!: number;
@@ -23,7 +23,7 @@ export class InmueblesComponent {
     @Inject('CONFIG_PAGINATOR') public configPaginator: PaginationInstance,
     private spinnerService: NgxSpinnerService,
     private mensajeService: MensajeService,
-    private formBuilder: FormBuilder,
+    private formBuilder: FormBuilder
   ) {
     this.creteForm();
   }
@@ -37,33 +37,36 @@ export class InmueblesComponent {
           Validators.maxLength(22),
           Validators.minLength(2),
           Validators.required,
-      ]
-    ],
-    cantidad: [
-      '',
-      [
-        Validators.maxLength(10),
-        Validators.required,
-      ]
-    ],
-    descripcion: [
-      '',
-      [Validators.required]
-    ],
-    imagen: [
-      '',
-      [Validators.required]
-    ],
-    areasDeResgualdo: [
-      '',
-      [Validators.required]
-    ]
-
+        ],
+      ],
+      cantidad: ['', [Validators.maxLength(10), Validators.required]],
+      descripcion: ['', [Validators.required]],
+      imagenBase64: [''],
+      areasDeResgualdo: ['', [Validators.required]],
     });
   }
 
   resetForm() {
     this.closebutton.nativeElement.click();
     this.inmueblesForm.reset();
+  }
+  onFileChange(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+
+    if (inputElement.files && inputElement.files.length > 0) {
+      const file = inputElement.files[0];
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        const base64String = reader.result as string;
+        const base64WithoutPrefix = base64String.split(';base64,').pop() || '';
+
+        this.inmueblesForm.patchValue({
+          imagenBase64: base64WithoutPrefix, // Contiene solo la representación en base64
+        });
+      };
+      this.isUpdatingfoto = false;
+      reader.readAsDataURL(file);
+    }
   }
 }
