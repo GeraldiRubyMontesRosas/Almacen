4f26f2e5aeeb34d5107193c837f6b7e1f5bb47b9
GABRIEL <gabriel.ruiz@jprconsulting.com.mx>
2024-05-28 16:25:15 -0600

    camara de scaner

--- src/app/pages/panel/scanner/scanner.component.ts
+++ src/app/pages/panel/scanner/scanner.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-scanner',
   templateUrl: './scanner.component.html',
-  styleUrls: ['./scanner.component.css']
+  styleUrls: ['./scanner.component.css'],
 })
 export class ScannerComponent {
+  @ViewChild('previewVideo') previewVideo!: ElementRef<HTMLVideoElement>;
+  cameraActive: boolean = false;
+  mediaStream: MediaStream | null = null;
 
+  constructor() {}
+
+  ngAfterViewInit() {
+    this.startPreview();
+  }
+
+  onCodeResult(resultString: string) {
+    console.log(resultString);
+    this.searchInDatabase(resultString);
+  }
+
+  searchInDatabase(qrCode: string) {
+    // Implementa la llamada a tu API aquí
+  }
+
+  toggleCamera() {
+    if (this.cameraActive) {
+      this.stopPreview();
+    } else {
+      this.startPreview();
+    }
+    this.cameraActive = !this.cameraActive;
+  }
+
+  private startPreview() {
+    const videoElement = this.previewVideo.nativeElement;
+    navigator.mediaDevices
+      .getUserMedia({ video: { facingMode: 'environment' } })
+      .then((stream) => {
+        videoElement.srcObject = stream;
+        videoElement.play();
+        this.mediaStream = stream; // Guardar la referencia al stream
+      })
+      .catch((err) => console.error('Error al acceder a la cámara: ', err));
+  }
+
+  private stopPreview() {
+    if (this.mediaStream) {
+      const tracks = this.mediaStream.getTracks();
+      tracks.forEach((track) => track.stop());
+    }
+  }
 }
--- src/app/pages/panel/scanner/scanner.module.ts
+++ src/app/pages/panel/scanner/scanner.module.ts
@@ -1,17 +1,31 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ScannerRoutingModule } from './scanner-routing.module';
 import { ScannerComponent } from './scanner.component';
-
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
+import { AppComponent } from 'src/app/app.component';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 @NgModule({
-  declarations: [
-    ScannerComponent
-  ],
+  declarations: [ScannerComponent, ScannerComponent],
   imports: [
     CommonModule,
-    ScannerRoutingModule
-  ]
+    ScannerRoutingModule,
+    ZXingScannerModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SharedModule,
+    NgxSpinnerModule,
+    NgxPaginationModule,
+    NgSelectModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class ScannerModule { }
+export class ScannerModule {}
