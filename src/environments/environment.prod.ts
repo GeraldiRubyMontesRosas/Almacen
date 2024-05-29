130db9889df68c2ecfa0ba944f134b1ca813ea93
Ruby Montes <ruby.montes.rosas@gmail.com>
2024-05-29 13:03:45 -0600

    conexionApi

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://simpatizantes.somee.com/api',
+    apiUrl: 'https://localhost:7037/api',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7206/api',
+  apiUrl: 'https://localhost:7037/api',
 };
