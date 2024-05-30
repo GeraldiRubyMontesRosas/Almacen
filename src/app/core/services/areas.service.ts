c3d5755084988230bd237df295f18a3c12d1ff7f
nahuatlatofl <nahuatlatofl@gmail.com>
2024-05-30 10:59:56 -0600

    Creacion del servicio de areas :)

---
+++ src/app/core/services/areas.service.ts
@@ -0,0 +1,56 @@
+  import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HandleErrorService } from './handle-error.service';
+import { Subject } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { Area } from 'src/app/models/Area';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AreasService {
+  route = `${environment.apiUrl}/area`;
+  private _refreshListAreas$ = new Subject<Area | null>();
+  
+  constructor(
+    private http : HttpClient,
+    private handleErrorService: HandleErrorService
+  ) { }
+
+  get refreshListAreas() {
+    return this._refreshListAreas$;
+  }
+
+  getById(id: number){
+    return this.http.get<Area>(`${this.route}/obtener-datos`);
+  }
+
+  post(dto : Area) {
+    return this.http.post<Area>(`${this.route}/crear`, dto).pipe(
+      tap(() => {
+        this._refreshListAreas$.next(null);
+      }),
+      catchError(this.handleErrorService.handleError)
+    );
+  }
+
+  put(id: number, dto: Area){
+   return this.http.put<Area>(`${this.route}/actualizar/${id}`, dto).pipe(
+    tap(() => {
+      this._refreshListAreas$.next(null);
+    }),
+    catchError(this.handleErrorService.handleError)
+   );
+  }
+
+  delete(id: number) {
+    return this.http.delete(`${this.route}/eliminar/${id}`).pipe(
+      tap(() => {
+        this._refreshListAreas$.next(null);
+      }),
+      catchError(this.handleErrorService.handleError)
+    )
+  }
+}
