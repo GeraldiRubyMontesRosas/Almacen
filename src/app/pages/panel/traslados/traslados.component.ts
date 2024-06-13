34b43f69f2748de7fd6414c29ada112388f9ed86
ManeItsHere <men2a.mane@gmail.com>
2024-06-13 16:49:47 +0200

    xd

--- src/app/pages/panel/traslados/traslados.component.ts
+++ src/app/pages/panel/traslados/traslados.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, ElementRef, ViewChild} from '@angular/core';
+import { Component, Inject, ElementRef, ViewChild, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PaginationInstance } from 'ngx-pagination';
 import { NgxSpinnerService } from 'ngx-spinner';
@@ -19,56 +19,52 @@ import * as QRCode from 'qrcode-generator';
   templateUrl: './traslados.component.html',
   styleUrls: ['./traslados.component.css']
 })
-export class TrasladosComponent {
+export class TrasladosComponent implements OnInit {
   @ViewChild('searchItem') searchItem!: ElementRef;
   @ViewChild('closebutton') closebutton!: ElementRef;
+  @ViewChild('video', { static: false }) public video!: ElementRef;
+  @ViewChild('canvas', { static: false }) public canvas!: ElementRef;
+
   public isUpdatingfoto: boolean = false;
   public imgPreview: string = '';
   public QrPreview: string = '';
-  inmueblesForm!: FormGroup;
-  QrBase64!: string;
+  public QrBase64!: string;
   public isUpdatingImg: boolean = false;
   public isUpdatingEmblema: boolean = false;
-  isLoading = LoadingStates.neutro;
-  idUpdate!: number;
-  isModalAdd = true;
-  inmueble!: Inmueble;
-  inmuebles: Inmueble[] = [];
-  areas: Area[] = [];
-  inmuebleFilter: Inmueble[] = [];
-  imagenAmpliada: string | null = null;
-  estatusBtn = true;
-  verdadero = 'Activo';
-  falso = 'Inactivo';
-  estatusTag = this.verdadero;
-  sliderValue: number = 0;
-  ceil: number = 100;
-  sliderOptions: Options = {
+  public isLoading = LoadingStates.neutro;
+  public idUpdate!: number;
+  public isModalAdd = true;
+  public inmueble!: Inmueble;
+  public inmuebles: Inmueble[] = [];
+  public areas: Area[] = [];
+  public inmuebleFilter: Inmueble[] = [];
+  public imagenAmpliada: string | null = null;
+  public estatusBtn = true;
+  public verdadero = 'Activo';
+  public falso = 'Inactivo';
+  public estatusTag = this.verdadero;
+  public sliderValue: number = 0;
+  public ceil: number = 100;
+  public sliderOptions: Options = {
     floor: 0,
     ceil: this.ceil,
   };  
-  filteredInmuebles = [];
-
+  public filteredInmuebles = [];
   public cameraActive: boolean = false;
-  @ViewChild('video', { static: false })
-  public video!: ElementRef;
-  @ViewChild('canvas', { static: false })
-  public canvas!: ElementRef;
   public captures: Array<any> = [];
+  public inmueblesForm!: FormGroup;
+
   constructor(
     @Inject('CONFIG_PAGINATOR') public configPaginator: PaginationInstance,
     private spinnerService: NgxSpinnerService,
     private mensajeService: MensajeService,
     private formBuilder: FormBuilder,
     public inmueblesService: InmueblesService,
-    public areasService: AreasService,
-    private fb: FormBuilder
+    public areasService: AreasService
   ) {
-    this.inmueblesService.refreshListInmuebles.subscribe(() =>
-      this.getInmuebles()
-    );
+    this.inmueblesService.refreshListInmuebles.subscribe(() => this.getInmuebles());
     this.getInmuebles();
-    this.creteForm();
+    this.createForm();
     this.getAreas();
   }
 
@@ -81,18 +77,39 @@ export class TrasladosComponent {
     });
   }
 
+  createForm() {
+    this.inmueblesForm = this.formBuilder.group({
+      id: [null],
+      codigo: [''],
+      nombre: [''],
+      cantidad: ['', [Validators.maxLength(10), Validators.required]],
+      descripcion: [''],
+      imagenBase64: [''],
+      qrBase64: [''],
+      areasDeResgualdo: [null, Validators.required],
+      estatus: [true],
+      costoUnitario: [''],
+      inmueble: [''],
+    });
+  }
+
   filterInmuebles(areaId: number): void {
-    this.filteredInmuebles != this.inmuebles.filter(inmueble => inmueble.area?.id === areaId);
+    this.filteredInmuebles = this.inmuebles.filter(inmueble => inmueble.area?.id === areaId);
   }
 
   setEstatus() {
     this.estatusTag = this.estatusBtn ? this.verdadero : this.falso;
   }
+
   getAreas() {
     this.isLoading = LoadingStates.trueLoading;
     this.areasService.getAll().subscribe({
       next: (dataFromAPI) => {
         this.areas = dataFromAPI;
+        this.isLoading = LoadingStates.falseLoading;
+      },
+      error: () => {
+        this.isLoading = LoadingStates.errorLoading;
       },
     });
   }
@@ -111,22 +128,6 @@ export class TrasladosComponent {
     });
   }
 
-  creteForm() {
-    this.inmueblesForm = this.formBuilder.group({
-      id: [null],
-      codigo: [''],
-      nombre: [''],
-      cantidad: ['', [Validators.maxLength(10), Validators.required]],
-      descripcion: [''],
-      imagenBase64: [''],
-      qrBase64: [''],
-      areasDeResgualdo: [null, Validators.required],
-      estatus: [true],
-      costoUnitario:[''],
-      inmueble:[''],
-    });
-  }
-
   resetForm() {
     this.closebutton.nativeElement.click();
     this.inmueblesForm.reset();
@@ -142,38 +143,33 @@ export class TrasladosComponent {
       cantidad: dto.cantidad,
       descripcion: dto.descripcion,
       imagenBase64: '',
-      QrBase64: '',
+      qrBase64: '',
       estatus: dto.estatus,
       areasDeResgualdo: dto.area ? dto.area.id : null,
     });
-    console.log(this.inmueblesForm);
   }
 
   editarInmueble() {
     this.inmueble = this.inmueblesForm.value as Inmueble;
-    const inmueble = this.inmueblesForm.get('id')?.value;
-    const area = this.inmueblesForm.get('areasDeResgualdo')?.value;
+    const inmuebleId = this.inmueblesForm.get('id')?.value;
+    const areaId = this.inmueblesForm.get('areasDeResgualdo')?.value;
     const imagenBase64 = this.inmueblesForm.get('imagenBase64')?.value;
-    const QrBase64 = this.inmueblesForm.get('QrBase64')?.value;
-    console.log(imagenBase64);
-    console.log(QrBase64);
+    const qrBase64 = this.inmueblesForm.get('qrBase64')?.value;
 
     this.imgPreview = '';
     this.QrPreview = '';
 
-    this.inmueble.area = { id: area } as Area;
+    this.inmueble.area = { id: areaId } as Area;
 
-    if (!imagenBase64 && !QrBase64) {
+    if (!imagenBase64 && !qrBase64) {
       const formData = { ...this.inmueble };
 
       this.spinnerService.show();
 
-      this.inmueblesService.put(inmueble, formData).subscribe({
+      this.inmueblesService.put(inmuebleId, formData).subscribe({
         next: () => {
           this.spinnerService.hide();
-          this.mensajeService.mensajeExito(
-            'Inmueble actualizado correctamente'
-          );
+          this.mensajeService.mensajeExito('Inmueble actualizado correctamente');
           this.resetForm();
           this.configPaginator.currentPage = 1;
         },
@@ -182,16 +178,14 @@ export class TrasladosComponent {
           this.mensajeService.mensajeError(error);
         },
       });
-    } else if (imagenBase64 && QrBase64) {
-      const formData = { ...this.inmueble, imagenBase64, QrBase64 };
+    } else if (imagenBase64 && qrBase64) {
+      const formData = { ...this.inmueble, imagenBase64, qrBase64 };
       this.spinnerService.show();
 
-      this.inmueblesService.put(inmueble, formData).subscribe({
+      this.inmueblesService.put(inmuebleId, formData).subscribe({
         next: () => {
           this.spinnerService.hide();
-          this.mensajeService.mensajeExito(
-            'inmuebles actualizado correctamente'
-          );
+          this.mensajeService.mensajeExito('Inmueble actualizado correctamente');
           this.resetForm();
           this.configPaginator.currentPage = 1;
         },
@@ -201,9 +195,7 @@ export class TrasladosComponent {
         },
       });
     } else {
-      console.error(
-        'Error: No se encontró una representación válida en base64 de la imagen.'
-      );
+      console.error('Error: No se encontró una representación válida en base64 de la imagen.');
     }
   }
 
@@ -213,7 +205,7 @@ export class TrasladosComponent {
       () => {
         this.inmueblesService.delete(id).subscribe({
           next: () => {
-            this.mensajeService.mensajeExito('Candidato borrado correctamente');
+            this.mensajeService.mensajeExito('Inmueble borrado correctamente');
             this.configPaginator.currentPage = 1;
             this.searchItem.nativeElement.value = '';
           },
@@ -222,6 +214,7 @@ export class TrasladosComponent {
       }
     );
   }
+
   onFileChange(event: Event) {
     const inputElement = event.target as HTMLInputElement;
 
@@ -234,7 +227,7 @@ export class TrasladosComponent {
         const base64WithoutPrefix = base64String.split(';base64,').pop() || '';
 
         this.inmueblesForm.patchValue({
-          imagenBase64: base64WithoutPrefix, // Contiene solo la representación en base64
+          imagenBase64: base64WithoutPrefix,
         });
       };
       this.isUpdatingfoto = false;
@@ -248,203 +241,79 @@ export class TrasladosComponent {
     if (nombreControl) {
       const nombre = nombreControl.value.toUpperCase();
 
-      const letraAleatoria = String.fromCharCode(
-        65 + Math.floor(Math.random() * 26)
-      ).toUpperCase();
-      const numerosAleatorios = Array.from({ length: 3 }, () =>
-        Math.floor(Math.random() * 10)
-      ).join('');
+      const letraAleatoria = String.fromCharCode(65 + Math.floor(Math.random() * 26)).toUpperCase();
+      const numerosAleatorios = Array.from({ length: 4 }, () => Math.floor(Math.random() * 10)).join('');
+      const codigoGenerado = `${nombre.substring(0, 3)}${letraAleatoria}${numerosAleatorios}`;
 
-      // Obtener la fecha actual en formato DDMMYYYY
-      const fecha = new Date();
-      const dia = String(fecha.getDate()).padStart(2, '0');
-      const mes = String(fecha.getMonth() + 1).padStart(2, '0');
-      const año = fecha.getFullYear();
-      const fechaActual = `${dia}${mes}${año}`;
-
-      // Generar el ID con prefijo "MAG" y fecha al final
-      const codigo = `MAG${nombre.slice(
-        0,
-        3
-      )}${letraAleatoria}${numerosAleatorios}${fechaActual}`;
+      this.inmueblesForm.patchValue({
+        codigo: codigoGenerado,
+      });
 
       const qr = QRCode(0, 'H');
-      qr.addData(codigo);
+      qr.addData(codigoGenerado);
       qr.make();
+      const qrBase64 = qr.createDataURL();
 
-      const qrDataURL = qr.createDataURL(4);
-      const qrBase64 = qrDataURL.split(',')[1]; // Extraer solo la parte base64
+      const qrBase64WithoutPrefix = qrBase64.split(';base64,').pop() || '';
+      this.QrPreview = qrBase64;
+      this.QrBase64 = qrBase64WithoutPrefix;
 
-      console.log('idGenerado:', codigo);
-      console.log('qrBase64:', qrBase64);
-
-      // Asignar el valor de idGenerado y qrBase64 al formulario
-      this.inmueblesForm.patchValue({ codigo: codigo, qrBase64: qrBase64 });
+      this.inmueblesForm.patchValue({
+        qrBase64: this.QrBase64,
+      });
     }
   }
 
-  agregar() {
-    const inmuebleid = this.inmueblesForm.get('inmueble')?.value;
-    const areaId = this.inmueblesForm.get('areasDeResgualdo')?.value;
-    const cantidad = this.inmueblesForm.get('cantidad')?.value;
-  
-    // Buscar el nombre del área seleccionada
-    const areaSeleccionada = this.areas.find((area) => area.id === areaId);
-    if (!areaSeleccionada) {
-      this.mensajeService.mensajeError(
-        'El área de resguardo seleccionada no es válida.'
-      );
-      return;
-    }
-  
-    // Buscar el inmueble seleccionado
-    const inmuebleSeleccionada = this.inmuebles.find((inmueble) => inmueble.id === inmuebleid);
-    if (!inmuebleSeleccionada) {
-      this.mensajeService.mensajeError(
-        'El inmueble de resguardo seleccionado no es válido.'
-      );
-      return;
-    }
-  
-    // Reemplazar el área en el objeto inmueble con el área seleccionada y establecer id como null
-    const inmuebleConIdNulo = { ...inmuebleSeleccionada,id: 0, area: areaSeleccionada, cantidad: cantidad };
-  
-    console.log(inmuebleConIdNulo);
-  
-    this.spinnerService.show();
-    this.inmueblesService.post(inmuebleConIdNulo).subscribe({
-      next: () => {
-        this.spinnerService.hide();
-        this.mensajeService.mensajeExito('Inmueble guardado correctamente');
-        this.resetForm();
-        this.configPaginator.currentPage = 1;
-      },
-      error: (error) => {
-        this.spinnerService.hide();
-        this.mensajeService.mensajeError(error);
-      },
-    });
-  }
-  
-  
-  handleChangeAdd() {
-    this.isUpdatingImg = false;
-    this.isUpdatingEmblema = false;
-    if (this.inmueblesForm) {
-      this.inmueblesForm.reset();
-      const estatusControl = this.inmueblesForm.get('estatus');
-      if (estatusControl) {
-        estatusControl.setValue(true);
-      }
-      this.isModalAdd = true;
+  activarCamara() {
+    this.cameraActive = true;
+
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+        const video = this.video.nativeElement;
+        video.srcObject = stream;
+        video.play();
+      });
     }
   }
 
-  submit() {
-    if (this.isModalAdd === false) {
-      this.editarInmueble();
-    } else {
-      this.agregar();
+  capturarImagen() {
+    const context = this.canvas.nativeElement.getContext('2d');
+    context.drawImage(this.video.nativeElement, 0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+
+    const imageData = this.canvas.nativeElement.toDataURL('image/png');
+    this.captures.push(imageData);
+    this.imgPreview = imageData;
+  }
+
+  cerrarCamara() {
+    this.cameraActive = false;
+
+    const video = this.video.nativeElement;
+    const stream = video.srcObject as MediaStream;
+
+    if (stream) {
+      const tracks = stream.getTracks();
+      tracks.forEach(track => track.stop());
     }
   }
 
-  mostrarImagenAmpliada(rutaImagen: string) {
-    this.imagenAmpliada = rutaImagen;
-    const modal = document.getElementById('modal-imagen-ampliada');
-    if (modal) {
-      modal.classList.add('show');
-      modal.style.display = 'block';
-    }
-  }
-  cerrarModal() {
-    this.imagenAmpliada = null;
-    const modal = document.getElementById('modal-imagen-ampliada');
-    if (modal) {
-      modal.classList.remove('show');
-      modal.style.display = 'none';
-    }
-  }
-  handleChangeSearch(event: any) {
-    const inputValue = event.target.value;
-    const valueSearch = inputValue.toLowerCase();
+  cargarInmueblesDesdeExcel(event: Event) {
+    const input = event.target as HTMLInputElement;
 
-    this.inmuebleFilter = this.inmuebles.filter(
-      (inmueble) =>
-        inmueble.nombre.toLowerCase().includes(valueSearch) ||
-        inmueble.codigo.toLowerCase().includes(valueSearch) ||
-        inmueble.cantidad.toString().includes(valueSearch)
-    );
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+      const reader = new FileReader();
 
-    this.configPaginator.currentPage = 1;
-  }
-  onPageChange(number: number) {
-    this.configPaginator.currentPage = number;
-  }
-  exportarDatosAExcel() {
-    if (this.areas.length === 0) {
-      console.warn('La lista de areas está vacía. No se puede exportar.');
-      return;
-    }
+      reader.onload = (e: ProgressEvent<FileReader>) => {
+        const data = new Uint8Array(e.target?.result as ArrayBuffer);
+        const workbook = XLSX.read(data, { type: 'array' });
+        const firstSheet = workbook.Sheets[workbook.SheetNames[0]];
+        const jsonData = XLSX.utils.sheet_to_json(firstSheet, { header: 1 });
 
-    const datosParaExportar = this.areas.map((area) => {
-      const estatus = area.estatus ? 'Activo' : 'Inactivo';
-      return {
-        '#': area.id,
-        'Nombre de area': area.nombre,
-        Responsable: area.responsable,
-        Estatus: estatus,
+        console.log(jsonData);
       };
-    });
 
-    const worksheet: XLSX.WorkSheet =
-      XLSX.utils.json_to_sheet(datosParaExportar);
-    const workbook: XLSX.WorkBook = {
-      Sheets: { data: worksheet },
-      SheetNames: ['data'],
-    };
-    const excelBuffer: any = XLSX.write(workbook, {
-      bookType: 'xlsx',
-      type: 'array',
-    });
-
-    this.guardarArchivoExcel(excelBuffer, 'Areas.xlsx');
-  }
-
-  guardarArchivoExcel(buffer: any, nombreArchivo: string) {
-    const data: Blob = new Blob([buffer], {
-      type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-    });
-    const url: string = window.URL.createObjectURL(data);
-    const a: HTMLAnchorElement = document.createElement('a');
-    a.href = url;
-    a.download = nombreArchivo;
-    a.click();
-    window.URL.revokeObjectURL(url);
-  }
-  mostrarImagenAmpliada2(urlImagen: string) {
-    const imagen = new Image();
-    imagen.onload = () => {
-      const canvas = document.createElement('canvas');
-      const context = canvas.getContext('2d');
-
-      if (context) {
-        canvas.width = imagen.width;
-        canvas.height = imagen.height;
-        context.drawImage(imagen, 0, 0);
-
-        const impresora = window.open('', '_blank');
-        if (impresora) {
-          impresora.document.write(
-            `<img src="${urlImagen}" style="max-width: 100%; max-height: 100%;" />`
-          );
-          impresora.document.write('<script>window.print();</script>');
-        } else {
-          console.error('No se pudo abrir la ventana de impresión.');
-        }
-      } else {
-        console.error('No se pudo obtener el contexto 2D del lienzo.');
-      }
-    };
-    imagen.src = urlImagen;
+      reader.readAsArrayBuffer(file);
+    }
   }
 }
