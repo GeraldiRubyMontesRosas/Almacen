65a6434da7e615c5133ebac70229d3d9a29011bd
nahuatlatofl <nahuatlatofl@gmail.com>
2024-06-17 10:36:30 -0600

    arreglando el scanner

--- src/app/pages/panel/scanner/scanner.component.ts
+++ src/app/pages/panel/scanner/scanner.component.ts
@@ -1,4 +1,7 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild,OnInit } from '@angular/core';
+import { FormBuilder, FormGroup} from '@angular/forms';
+import { InmueblesService } from 'src/app/core/services/inmueble.service';
+import { Inmueble } from 'src/app/models/inmueble';
 
 @Component({
   selector: 'app-scanner',
@@ -9,47 +12,22 @@ export class ScannerComponent {
   @ViewChild('previewVideo') previewVideo!: ElementRef<HTMLVideoElement>;
   cameraActive: boolean = false;
   mediaStream: MediaStream | null = null;
+  
+  inmueble !: Inmueble;
+  inmuebles : Inmueble[] = [];
+  inmueblesForm !: FormGroup;
+  inmueblesFilter: Inmueble[] = [];
 
-  constructor() {}
-
-  ngAfterViewInit() {
-    this.startPreview();
-  }
-
-  onCodeResult(resultString: string) {
-    console.log(resultString);
-    this.searchInDatabase(resultString);
-  }
-
-  searchInDatabase(qrCode: string) {
-    // Implementa la llamada a tu API aquí
-  }
+  constructor(
+    private formBuilder: FormBuilder,
+    private inmueblesService: InmueblesService 
+  ) {}
 
   toggleCamera() {
-    if (this.cameraActive) {
-      this.stopPreview();
-    } else {
-      this.startPreview();
-    }
     this.cameraActive = !this.cameraActive;
   }
 
-  private startPreview() {
-    const videoElement = this.previewVideo.nativeElement;
-    navigator.mediaDevices
-      .getUserMedia({ video: { facingMode: 'environment' } })
-      .then((stream) => {
-        videoElement.srcObject = stream;
-        videoElement.play();
-        this.mediaStream = stream; // Guardar la referencia al stream
-      })
-      .catch((err) => console.error('Error al acceder a la cámara: ', err));
-  }
-
-  private stopPreview() {
-    if (this.mediaStream) {
-      const tracks = this.mediaStream.getTracks();
-      tracks.forEach((track) => track.stop());
-    }
+  handleScanSuccess(result: string) {
+    console.log('Scan result:', result);
   }
 }
